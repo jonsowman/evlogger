9ec3abde1aabb19806f7d8c13b06de7c150cdba8
Jon Sowman <jon@hexoc.com>
2013-11-16 13:20:44 +0000

    Moved delay function into a module so can be used elsewhere

---
+++ delay.c
@@ -0,0 +1,22 @@
+/**
+ * EV Datalogger Project
+ *
+ * Jon Sowman 2013 <js39g13@soton.ac.uk>
+ * University of Southampton
+ */
+
+#include "delay.h"
+
+/**
+ * Delay for the provided number of milliseconds
+ * \param delay The number of ms to delay.
+ */ 
+void _delay_ms(uint32_t delay)
+{
+    uint32_t i;
+    for(i=0; i < delay; i++)
+    {
+        __delay_cycles(20000);
+    }
+}
+
---
+++ delay.h
@@ -0,0 +1,16 @@
+/**
+ * EV Datalogger Project
+ *
+ * Jon Sowman 2013 <js39g13@soton.ac.uk>
+ * University of Southampton
+ */
+
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+#include <stdio.h>
+#include "typedefs.h"
+
+void _delay_ms(uint32_t delay);
+
+#endif /* __DELAY_H__ */
